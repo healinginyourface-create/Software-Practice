b4b51d2c7948b42c0013fd4df676a2f6aeb71a4b
Hyunho <healinginyourface@gmail.com>
2026-08-03 17:04:00 +0900

    Learn Python if statement

---
+++ Python/if.py
@@ -0,0 +1,6 @@
+speed = 85
+
+if speed >= 80:
+    print("속도가 너무 빠릅니다.")
+
+print("주행 중...")
---
+++ Python/variables.py
@@ -0,0 +1,14 @@
+name = "Hyun"
+age = 23
+height = 183
+weight = 75
+
+print("이름 :", name)
+print("나이 :", age)
+print("키 :", height)
+print("몸무게 :", weight)
+
+birth = 2003
+current = 2026
+
+print("계산된 나이 :", current - birth)
