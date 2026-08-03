a3aeb3e021a55bdf4ef44e082a709b26579ed0e7
Hyunho <healinginyourface@gmail.com>
2026-08-03 17:21:20 +0900

    Day 5 - Learn NumPy basics

---
+++ Python/NumPy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([10, 20, 30])
+
+print(a)
+
+print(a * 2)    
---
+++ Python/function.py
@@ -0,0 +1,17 @@
+def hello():
+    print("안녕하세요!")
+
+hello()
+
+
+def introduce(name):
+    print("이름:", name)
+
+introduce("Hyun")
+
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print(result)
